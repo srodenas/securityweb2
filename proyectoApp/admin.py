fc85b21d48fb03687c9a4e7bba02e0b4c5abada0
santi <srodher115@g.educaand.es>
2022-02-10 09:18:48 +0100

    Registramos nuestros modelos en admin y creamos usuario admin

--- proyectoApp/admin.py
+++ proyectoApp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display=('nombre','apellido','email')
+    search_fields = ['nombre']
+    
+class ArticuloAdmin(admin.ModelAdmin):
+    list_display=('descripcion',)
+    search_fields = ['descripcion']
+    
+
+#admin.site.register(Cliente)
+#admin.site.register(Articulo)
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.register(Articulo, ArticuloAdmin)
